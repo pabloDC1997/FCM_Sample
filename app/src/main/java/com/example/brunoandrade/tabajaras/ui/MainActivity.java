456f6236525935dad0800583adcbb3d2f6f1bd50
pablo.couto <pablo.couto@pcinformatica.com.br>
2017-09-01 14:53:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/brunoandrade/tabajaras/ui/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.brunoandrade.tabajaras.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.brunoandrade.tabajaras.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+        return true;
+    }
+}
